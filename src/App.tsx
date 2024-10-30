22b61e6c139b473996adb0d06cf24e1c01fea20f
LucasBbe <lucas.boube@gmail.com>
2024-10-30 10:45:10 +0100

    etape 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 
+const liens = {
+  boxShadow: "black 1px 2px 5px",
+  margin: "10px",
+  textAligns: "center",
+  textDecoration: "none",
+  padding: "5px",
+};
+
 function App() {
   return (
     <>
-      <nav className="navBar">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <nav>
+        <Link style={liens} to="/">
+          {" "}
+          Home
+        </Link>
+        <Link style={liens} to="/about">
+          {" "}
+          About
+        </Link>
+        <Link style={liens} to="/articles/1">
+          {" "}
+          Article 1
+        </Link>
+        <Link style={liens} to="/articles/2">
+          {" "}
+          Article 2
+        </Link>
+        <Link style={liens} to="/articles/3">
+          {" "}
+          Article 3
+        </Link>
       </nav>
       <main>
         <Outlet />
